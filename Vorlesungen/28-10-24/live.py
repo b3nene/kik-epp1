0607e4cba1573ecf7d40711af9271f70d1021519
bene <ben.ege@gmx.de>
2024-10-28 15:37:26 +0100

    feat: decision tree classifier

--- Vorlesungen/28-10-24/live.py
+++ Vorlesungen/28-10-24/live.py
@@ -1,21 +1,33 @@
 import numpy as np
+import sklearn.tree
+from sklearn.tree import plot_tree
 
 data = np.loadtxt("/home/bene/Documents/Code/kik-epp1/Vorlesungen/28-10-24/data.csv", delimiter=",", skiprows=1)
 
-x = data[ : ,0:3 ] # : -> von bis (exklusiv)
+x = data[ : ,0:2] # : -> von bis (exklusiv)
 #print (x)
 
 y = data[: , 2]
 
 #print (y)
 
-for i in range(x.shape[0]):
-    print ("Datapoint: ",i)
-    print("A,H,OS",data[i])
-    print("age: ", x[i,0])
-    print("height:", x[i,1])
-    if x[i,2]==0:
-        print ("android")
-    else:
-        print("iOs")
+#for i in range(x.shape[0]):
+ #   print ("Datapoint: ",i)
+  #  print("A,H,OS",data[i])
+   # print("age: ", x[i,0])
+    #print("height:", x[i,1])
+    #if y[i,2]==0:
+     #   print ("android")
+    #else:
+     #   print("iOs")
     
+decision_tree = sklearn.tree.DecisionTreeClassifier(criterion="gini")
+decision_tree.fit(x,y)
+
+prediction = [36, 176]
+nppred = np.array(prediction)
+rnppred= nppred.reshape(1, -1)
+
+print(
+    decision_tree.predict(rnppred)
+)
