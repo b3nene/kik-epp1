0208ca2ee3b09cbac074f8877d40efaa9c013253
bene <ben.ege@gmx.de>
2024-10-28 15:10:58 +0100

    feat: multi print

--- Vorlesungen/28-10-24/live.py
+++ Vorlesungen/28-10-24/live.py
@@ -2,7 +2,20 @@ import numpy as np
 
 data = np.loadtxt("/home/bene/Documents/Code/kik-epp1/Vorlesungen/28-10-24/data.csv", delimiter=",", skiprows=1)
 
-x = data[ : ,0:2 ] # : -> von bis (exklusiv)
-print (x)
+x = data[ : ,0:3 ] # : -> von bis (exklusiv)
+#print (x)
 
-y = x
+y = data[: , 2]
+
+#print (y)
+
+for i in range(x.shape[0]):
+    print ("Datapoint: ",i)
+    print("A,H,OS",data[i])
+    print("age: ", x[i,0])
+    print("height:", x[i,1])
+    if x[i,2]==0:
+        print ("android")
+    else:
+        print("iOs")
+    
