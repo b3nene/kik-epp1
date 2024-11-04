faf887bb26c9076690cc5726d8d3d577d8342c93
bene <ben.ege@gmx.de>
2024-11-04 15:25:49 +0100

    feat: tuple and dictionary

--- Vorlesungen/04-11-2024/live-uebung.py
+++ Vorlesungen/04-11-2024/live-uebung.py
@@ -9,3 +9,12 @@ print(shapes)
 tupleshape = ("square","circle","pentagon")
 
 print(tupleshape)
+
+shape_parameters = {
+    "square": 1.0,
+    "circle": 3.5,
+    "triangle": (2.0,3.0,42)
+}
+
+print(shape_parameters)
+print(shape_parameters["circle"])
